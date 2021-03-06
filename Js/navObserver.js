b4b341edfcd9933a2f2ced64091dc86866452489
em40250 <em40250@ubt-uni.net>
2021-03-06 16:43:18 +0100

    navbar with observer, contents added

---
+++ Js/navObserver.js
@@ -0,0 +1,24 @@
+const sectionOne = document.querySelector(".header");
+const logoImg = document.querySelector(".logoImg");
+const navBtn = document.querySelector(".navBtn");
+const nav = document.querySelector(".nav");
+
+const sectionOneOptions = {
+    rootMargin: "-200px 0px 0px 0px"
+};
+
+const sectionOneObesrver = new IntersectionObserver(function(entries,sectionOneOptions){
+    entries.forEach(entry => {
+       if(!entry.isIntersecting){
+            logoImg.classList.remove("hidden");
+            navBtn.classList.remove("marginNav");
+            nav.classList.add("navBg");
+       } else {
+        logoImg.classList.add("hidden");
+        navBtn.classList.add("marginNav");
+        nav.classList.remove("navBg");
+       }
+    });
+}, sectionOneOptions);
+
+sectionOneObesrver.observe(sectionOne);
